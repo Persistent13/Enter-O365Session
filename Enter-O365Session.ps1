f38dec802ff856a0acf1d94a1ff78b12fa05be5e
Persistent13 <dakota@allclarks.com>
2014-07-08 17:26:18 -0700

    Created the Office 365 connection script and the readme file.

---
+++ Enter-O365Session.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = "Stop"
+do{
+    try{
+        $strOk = $true
+        $LiveCred = Read-Host 'Please enter the username "user@example.com".'
+    }
+    catch{
+        $strOk = $false
+    }
+}
+until(($LiveCred -match "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b") -and $strOk)
+try{
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+    Import-PSSession $Session
+}
+catch [Exception]{
+    Write-Host "$($_.Exception.Message) Be sure you entered your credentials correctly." -ForegroundColor Red
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+WHO:
+	Dakota Clark <dclark@team-sos.com>
+WHAT:
+	To connect to a Office 365 environment in a one liner.
+HOW:
+	Using the Enter-PSSession and tip from the Oulook.com
+	website from PS session management.
+	http://help.outlook.com/en-us/140/cc952755.aspx
+ACTIONS:
+	The script requests a username in the format of
+	user@example.com and then connects to the O365 cloud
+	if the credentials given are correct. It will print the
+	system error if the connection was a failure.
+REQUIREMENTS:
+	Powershell v2
+	Office 365 account
