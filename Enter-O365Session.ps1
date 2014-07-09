27887e1002a6136e92e45595eca831b86fcdb541
Dakota Clark <dakota@allclarks.com>
2014-07-09 08:27:25 -0700

    Added some extra detail and a command to exit the imported session. The MIT license has been added as well.

--- Enter-O365Session.ps1
+++ Enter-O365Session.ps1
@@ -12,6 +12,11 @@ until(($LiveCred -match "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b") -and $strO
 try{
     $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
     Import-PSSession $Session
+    Write-Host " "
+    Write-Host "Connected!" -ForegroundColor Green
+    Write-Host "Type the command"
+    Write-Host "    Remove-PSSession $((Get-PSSession).Id)" -ForegroundColor Yellow
+    Write-Host "to exit the session."
 }
 catch [Exception]{
     Write-Host "$($_.Exception.Message) Be sure you entered your credentials correctly." -ForegroundColor Red
