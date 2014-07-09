aa7fe06db9487471c5e26effc283d2d5a5df8fb6
Persistent13 <dakota@allclarks.com>
2014-07-09 16:56:59 -0700

    The params now work as intended.

--- Enter-O365Session.psm1
+++ Enter-O365Session.psm1
@@ -1,38 +1,40 @@
 ﻿function Enter-O365Session{
-param(
-    [Parameter(Mandatory=$false)]
-    [Alias("u")]
-    [String]$liveUser,
-    [Parameter(Mandatory=$false)]
-    [Alias("p")]
-    [Security.SecureString]$livePassword
-)
-$ErrorActionPreference = "Stop"
-        if(($liveUser -contains $null) -and $liveCredential -notmatch "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b"){
-            do{
-                try{
-                    $secureStringOk = $true
-                    $liveUser = Read-Host 'Please enter the username. "foo@example.com"'
-                }
-                catch{
-                    $secureStringOk = $false
-                }
+    param(
+        [Parameter(Mandatory=$false)]
+        [ValidatePattern("\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b")]
+        $User,
+        [Parameter(Mandatory=$false)]
+        $Password
+    )
+    $ErrorActionPreference = "Stop"
+    if(!$User){
+        do{
+            try{
+                $secureStringOk = $true
+                $User = Read-Host 'Please enter the username. "foo@example.com"'
             }
-            until(($liveCredential -match "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b") -and $secureStringOk)
+            catch{
+                $secureStringOk = $false
+            }
         }
-        if($livePassword -contains $null){
-            $livePassword = Read-Host -AsSecureString "Please enter the password."
-        }
+        until(($User -match "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b") -and $secureStringOk)
+    }
+    if($Password.Length -gt 0){
+        $Password = $Password | ConvertTo-SecureString -AsPlainText -Force
+    }
+    if(!$Password){
+        $Password = Read-Host -AsSecureString "Please enter the password"
+    }
+    $liveCredential = New-Object -Typename System.Management.Automation.PSCredential -ArgumentList $User,$Password
     try{
         $liveSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $liveCredential -Authentication Basic -AllowRedirection
-        Import-PSSession $liveSession
-        Write-Output " "
-        Write-Output "Connected!" -ForegroundColor Green
-        Write-Output "Type the command"
-        Write-Output "    Remove-PSSession $((Get-PSSession).Id)" -ForegroundColor Yellow
-        Write-Output "to exit the session."
+        Import-PSSession -AllowClobber $liveSession
+        Write-Host "
+        "
+        Write-Output "Exit with command Remove-PSSession $((Get-PSSession).Id)"
+
     }
     catch [Exception]{
-        Write-Output "$($_.Exception.Message) Be sure you entered your credentials correctly." -ForegroundColor Red
+        Write-Output "$($_.Exception.Message)"
     }
 }
